64d673213d7e43500bfb48d7fa5af9e77dcee192
DucAnhPhi <ducanhphi1@gmail.com>
2020-12-01 18:56:00 +0100

    extract text from article with test

--- feature_extraction.py
+++ feature_extraction.py
@@ -2,8 +2,7 @@ import re
 
 class FeatureExtraction():
 
-    @staticmethod
-    def get_first_paragraph(article):
+    def get_first_paragraph(self, article):
         body = article["body"]
         iter = re.finditer(r"\n", body)
         offset = next(iter).start()
@@ -12,3 +11,14 @@ class FeatureExtraction():
         first_p = body.replace("\n", " ")[:offset]
         first_p = first_p.replace("  ", " ")
         return first_p
+
+    def get_first_paragraph_with_titles(self, article):
+        first_p = self.get_first_paragraph(article)
+        title = article["title"]
+        subtitle = article["subtitle"]
+        text = ""
+        if subtitle is None:
+            text += title.strip() + " " + first_p.strip()
+        else:
+            text += subtitle.strip() + " " + title.strip() + " " + first_p.strip()
+        return text
--- test/test_feature_extraction.py
+++ test/test_feature_extraction.py
@@ -19,8 +19,28 @@ class TestFeatureExtraction(unittest.TestCase):
         with open("test/jsonl/test_articles.jsonl", "r") as f:
             for line in f:
                 article = json.loads(line)
-                first_paragraph = FeatureExtraction.get_first_paragraph(article)
+                fe = FeatureExtraction()
+                first_paragraph = fe.get_first_paragraph(article)
                 actual_text.append(first_paragraph)
         
+        for id, actual in enumerate(actual_text):
+            self.assertEqual(actual, expected_text[id])
+
+    def test_get_query_text(self):
+        actual_text = []
+        expected_text = [
+            "Karlsruhe Energieversorger plant automatisierte Videoüberwachung Die Stadt Karlsruhe will das „Sicherheitsgefühl“ in der Innenstadt verbessern. Die rechtlichen Voraussetzungen für eine polizeiliche Videoüberwachung sind nicht erfüllt. Eine Entwicklung des Energiekonzerns EnBW könnte diese Hürde umgehen.",
+            "#netzrückblick: 32C3 – der zweite Tag Der zweite Tag des 32. Chaos Communication Congress ist noch nicht ganz vorüber, aber wir werfen schon mal einen Blick auf die bisherigen Presseberichte und haben ein paar Empfehlungen für den morgigen, dritten Tag zusammengestellt.",
+            "Entscheide dich und bleib dabei. Erlebnisbericht eines Neulings auf dem Chaos Communication Congress Teil I: Es gibt viele Gründe, zum Chaos Communication Congress zu fahren. Ebenso viele sprechen dagegen. Vermutlich stimmt beides. Ein Erlebnisbericht von ebenjenem.",
+            "Ein Text, den ich noch nicht lesen kann. Erlebnisbericht eines Neulings auf dem Chaos Communication Congress Teil II Was ist das? Was sagt er da? Was machen die da? Der Congress-Neuling schnappt Dinge auf, hört zu und beobachtet. Es ergibt sich ein Text, der nicht all zu leicht zu verstehen ist."
+        ]
+
+        with open("test/jsonl/test_articles.jsonl", "r") as f:
+            for line in f:
+                article = json.loads(line)
+                fe = FeatureExtraction()
+                q_text = fe.get_first_paragraph_with_titles(article)
+                actual_text.append(q_text)
+
         for id, actual in enumerate(actual_text):
             self.assertEqual(actual, expected_text[id])
