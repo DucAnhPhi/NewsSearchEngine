3ff3ab681a479dfac2229a881070951e7b0a27d1
DucAnhPhi <ducanhphi1@gmail.com>
2020-12-18 12:03:58 +0100

    make feature extraction module generic

--- feature_extraction.py
+++ feature_extraction.py
@@ -1,13 +1,29 @@
+import re
+from typings import Vector, StringList
 from scipy.spatial.distance import cosine
 import numpy as np
 from itertools import combinations
 
 class FeatureExtraction():
 
+    def __init__(self, embedder, parser):
+        self.embedder = embedder
+        self.parser = parser
+
     @staticmethod
-    def mean_of_pairwise_cosine_distances(self, embeddings) -> float:
+    def mean_of_pairwise_cosine_distances(embeddings) -> float:
         # all combinations of 2 rows, ignoring order and no repeated elements
         coms = combinations(embeddings, 2)
         dists = np.array([cosine(pair[0], pair[1]) for pair in coms])
         mean_dist = np.mean(dists)
         return mean_dist
+
+    def get_first_paragraph_with_titles_embedding(self, article) -> Vector:
+        combined_text = self.parser.get_first_paragraph_with_titles(article)
+        return self.embedder.encode(combined_text)
+
+    def get_semantic_specifity(self, article):
+        text_tokens = self.parser.get_section_titles(article)
+        token_embeddings = [self.embedder.encode(token) for token in text_tokens]
+        semantic_specifity = FeatureExtraction.mean_of_pairwise_cosine_distances(token_embeddings)
+        return semantic_specifity
--- test/test_feature_extraction.py
+++ test/test_feature_extraction.py
@@ -2,15 +2,10 @@ import sys
 sys.path.append("..")
 import unittest
 import json
-import os
 from feature_extraction import FeatureExtraction
-from embedding.model import EmbeddingModel
 import numpy as np
 
 class TestFeatureExtraction(unittest.TestCase):
-    def setUp(self):
-        self.embedder = EmbeddingModel()
-        self.fe = FeatureExtraction(self.embedder)
 
     def test_mean_of_pairwise_cosine_distances(self):
         ems = np.array([
@@ -18,25 +13,4 @@ class TestFeatureExtraction(unittest.TestCase):
             [-11,3,9],
             [22,0,8]
         ], dtype=float)
-        self.assertTrue(abs(0.9770-self.fe.mean_of_pairwise_cosine_distances(ems)) < 1e-4)
-
-    def test_semantic_specifity(self):
-        article_sim = {
-            "raw_body": '''
-                <h3>Huhn</h3>
-                <h3>Ei</h3>
-                <h3>Vogel</h3>
-                <h3>Geflügel</h3>
-            '''
-        }
-        article_diff = {
-            "raw_body": '''
-                <h2>Code</h2>
-                <h2>Geflügel</h2>
-                <h2>Siebträger</h2>
-                <h2>Donald Trump</h2>
-            '''
-        }
-        ss_sim = self.fe.get_semantic_specifity(article_sim)
-        ss_diff = self.fe.get_semantic_specifity(article_diff)
-        self.assertTrue(ss_sim < ss_diff)
+        self.assertTrue(abs(0.9770 - FeatureExtraction.mean_of_pairwise_cosine_distances(ems)) < 1e-4)
