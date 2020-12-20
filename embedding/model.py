935b21464d8ed6c2b1b39dc3b368d2c8585c93b3
DucAnhPhi <ducanhphi1@gmail.com>
2020-12-20 11:51:34 +0100

    use cpu if gpu vram is not sufficient

--- embedding/model.py
+++ embedding/model.py
@@ -15,27 +15,26 @@ class EmbeddingModel():
 
         handle = nvidia_smi.nvmlDeviceGetHandleByIndex(0)
         free_memory = nvidia_smi.nvmlDeviceGetMemoryInfo(handle).free
+        device = "cpu"
 
-        # If you run into GPU memory problems, uncomment the following line:
-
-        # os.environ["CUDA_VISIBLE_DEVICES"]=""
-
-        # disable GPU usage if VRAM < 1.5GB
-        if free_memory < 1500000000:
-            os.environ["CUDA_VISIBLE_DEVICES"]=""
+        # disable GPU usage if VRAM < 2GB
+        if free_memory > 2e9:
+            if torch.cuda.is_available():
+                device = "cuda"
 
         # load BERT model from Hugging Face
-        word_embedding_model = models.Transformer(
-        'bert-base-german-cased')
+        word_embedding_model = models.Transformer('bert-base-german-cased')
 
         # Apply mean pooling to get one fixed sized sentence vector
-        pooling_model = models.Pooling(word_embedding_model.get_word_embedding_dimension(),
-                                pooling_mode_mean_tokens=True,
-                                pooling_mode_cls_token=False,
-                                pooling_mode_max_tokens=False)
+        pooling_model = models.Pooling(
+            word_embedding_model.get_word_embedding_dimension(),
+            pooling_mode_mean_tokens=True,
+            pooling_mode_cls_token=False,
+            pooling_mode_max_tokens=False
+        )
 
         # join BERT model and pooling to get the sentence transformer
-        self.model = SentenceTransformer(modules=[word_embedding_model, pooling_model])
+        self.model = SentenceTransformer(modules=[word_embedding_model, pooling_model], device = device)
         self.model.max_seq_length = 512
 
     def encode(self, text:str) -> Vector:
