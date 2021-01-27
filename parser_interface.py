c9f6dcf4193f638c074fac5edf8ec8d2990c3ae5
DucAnhPhi <ducanhphi1@gmail.com>
2021-01-27 19:18:46 +0100

    update interface

--- parser_interface.py
+++ parser_interface.py
@@ -6,12 +6,8 @@ class ParserInterface(metaclass=abc.ABCMeta):
         return (
             hasattr(subclass, 'get_first_paragraph_with_titles') and
             callable(subclass.get_first_paragraph_with_titles) and
-            hasattr(subclass, 'get_titles') and
-            callable(subclass.get_titles) and
             hasattr(subclass, 'get_keywords') and
-            callable(subclass.get_keywords) and
-            hasattr(subclass, 'parse_article') and
-            callable(subclass.parse_article) or
+            callable(subclass.get_keywords) or
             NotImplemented)
 
     @abc.abstractstaticmethod
@@ -19,13 +15,5 @@ class ParserInterface(metaclass=abc.ABCMeta):
         raise NotImplementedError
 
     @abc.abstractstaticmethod
-    def get_titles(article) -> str:
-        raise NotImplementedError
-
-    @abc.abstractstaticmethod
     def get_keywords(article) -> str:
-        raise NotImplementedError
-
-    @abc.abstractstaticmethod
-    def parse_article(response):
         raise NotImplementedError
