462e6ea888e4e3926b0e9d1b2f11b1b20b6eaa09
DucAnhPhi <ducanhphi1@gmail.com>
2021-01-28 20:51:34 +0100

    fix key errors

--- wapo/parser.py
+++ wapo/parser.py
@@ -108,7 +108,8 @@ class ParserWAPO(ParserInterface):
 
     @staticmethod
     def get_first_paragraph_with_titles(article) -> str:
-        first_p = article["_source"]["text"][0:article["offset_first_paragraph"]]
+        article = article["source"]
+        first_p = article["text"][0:article["offset_first_paragraph"]]
         title = article["title"]
         text = title.strip() + " " + first_p.strip()
         return text
