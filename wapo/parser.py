cdeb7267e5a62996b06385b1dfc54e2395cbd349
DucAnhPhi <ducanhphi1@gmail.com>
2021-01-28 20:17:41 +0100

    fix minor bugs in wapo parser and add unit tests

--- wapo/parser.py
+++ wapo/parser.py
@@ -6,15 +6,25 @@ class ParserWAPO(ParserInterface):
     @staticmethod
     def get_first_content_by_type(jsarr, t):
         for block in jsarr:
-            if block is not None and block['type'] == t:
-                return block['content']
+            if block is not None and block["type"] == t:
+                return block["content"]
         return None
 
     @staticmethod
-    def get_all_content_by_type(jsarr, t, field='content'):
-        strings = [c[field] for c in jsarr if c is not None and c['type'] == t and field in c and c[field] is not None]
+    def get_first_paragraph(jsarr):
+        first_p = ""
+        for block in jsarr:
+            if block is not None and block["type"] == "sanitized_html":
+                first_p += f"{block['content']} "
+                if block["subtype"] == "paragraph" and len(first_p) > 50:
+                    return first_p.strip()
+        return ""
+
+    @staticmethod
+    def get_all_content_by_type(jsarr, t, field="content"):
+        strings = [c[field] for c in jsarr if c is not None and c["type"] == t and field in c and c[field] is not None]
         if strings:
-            return ' '.join(strings)
+            return " ".join(strings)
         else:
             return None
 
@@ -45,59 +55,60 @@ class ParserWAPO(ParserInterface):
 
     def parse_article(self, raw):
         try:
-            text = ParserWAPO.get_all_content_by_type(js['contents'], 'sanitized_html')
-            first_p = ParserWAPO.get_first_content_by_type(js['contents'], 'sanitized_html')
+            text = ParserWAPO.get_all_content_by_type(raw['contents'], 'sanitized_html')
+            first_p = ParserWAPO.get_first_paragraph(raw['contents'])
+            first_p = re.sub('<.*?>', ' ', first_p)
+            first_p = first_p.strip()
             links = []
             if text:
                 links = re.findall('href="([^"]*)"', text)
                 text = re.sub('<.*?>', ' ', text)
-            title = ParserWAPO.get_all_content_by_type(js['contents'], 'title')
-            kicker = ParserWAPO.get_first_content_by_type(js['contents'], 'kicker')
+            text = text.strip()
+            title = ParserWAPO.get_all_content_by_type(raw['contents'], 'title')
+            title = title.strip()
+            kicker = ParserWAPO.get_first_content_by_type(raw['contents'], 'kicker')
 
             # ignore not relevant docs
-            if "published_date" not in js or not title.strip() or not text.strip() or ParserWAPO.is_not_relevant(kicker):
+            if "published_date" not in raw or not title or not text or ParserWAPO.is_not_relevant(kicker):
+                print("irrelevant")
                 return None
 
             data_dict = {
-                "_index": args.index_name,
+                "_index": "wapo",
                 "_type": '_doc',
-                "_id": js['id'],
+                "_id": raw['id'],
             }
 
-            keywords = ParserWAPO.get_keywords(text)
+            keywords = self.get_keywords(text)
 
             source_block = {
                 "title": title,
                 "offset_first_paragraph": len(first_p),
-                "date": js['published_date'],
+                "date": raw['published_date'],
                 "kicker": kicker,
-                "author": js['author'],
+                "author": raw['author'],
                 "text": text or '',
                 "links": links or [],
-                "url": js['article_url'],
+                "url": raw['article_url'],
                 "keywords": keywords
 
             }
 
-            for key, val in js.items():
+            for key, val in raw.items():
                 if key == key.upper():
-                    print(js['id'], ParserWAPO.unique_heads(val))
+                    print(raw['id'], ParserWAPO.unique_heads(val))
                     source_block[key] = ParserWAPO.unique_heads(val)
 
             data_dict['_source'] = source_block
 
-        except Exception:
+        except Exception(e):
             return None
 
         return data_dict
 
     @staticmethod
-    def get_first_paragraph(article) -> str:
-        return article["text"][0:len(article["offset_first_paragraph"])]
-
-    @staticmethod
     def get_first_paragraph_with_titles(article) -> str:
-        first_p = ParserWAPO.get_first_paragraph(article)
+        first_p = article["text"][0:article["offset_first_paragraph"]]
         title = article["title"]
         text = title.strip() + " " + first_p.strip()
         return text
