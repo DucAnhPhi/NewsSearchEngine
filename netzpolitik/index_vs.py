b56374052464d4b847300893c4ebf54723ad6f55
DucAnhPhi <ducanhphi1@gmail.com>
2021-02-14 11:50:07 +0100

    refactor vector storage index scripts

--- netzpolitik/index_vs.py
+++ netzpolitik/index_vs.py
@@ -8,43 +8,55 @@ import os
 if __name__ == "__main__":
     es = Elasticsearch()
     parser = ParserNetzpolitik(es)
-    em = EmbeddingModel()
+    lang = "de"
+    em = EmbeddingModel(lang)
     fe = FeatureExtraction(em, parser)
+    index = "netzpolitik"
     data_location = f"{os.path.abspath(os.path.join(__file__ , os.pardir, os.pardir))}/data"
     articles_path = f"{data_location}/netzpolitik.jsonl"
+    num_elements = 20000
 
-    print("Initialize netzpolitik vector storage of embeddings of titles w/ first paragraph.\n")
-    def embedding_func_twfp(raw):
+    print("Initialize netzpolitik vector storage of embeddings of title.\n")
+    def embedding_func_title(raw):
+        return fe.get_embedding_of_title(raw)
+    VectorStorage(f"{data_location}/netzpolitik_vs_title.bin", num_elements) \
+        .add_items_from_file(articles_path, embedding_func_title)
+
+    print("Initialize netzpolitik vector storage of embeddings of title and section titles.\n")
+    def embedding_func_title_with_section_titles(raw):
+        return fe.get_embedding_of_title_with_section_titles(raw)
+    VectorStorage(f"{data_location}/netzpolitik_vs_title_with_section_titles.bin", num_elements) \
+        .add_items_from_file(articles_path, embedding_func_title_with_section_titles)
+
+    print("Initialize netzpolitik vector storage of embeddings of title with first paragraph.\n")
+    def embedding_func_title_with_first_paragraph(raw):
         return fe.get_embedding_of_title_with_first_paragraph(raw)
-    vs_twfp = VectorStorage()
-    vs_twfp.add_items_from_file(articles_path, embedding_func_twfp)
-    vs_twfp.save(f"{data_location}/netzpolitik_vs_twfp.bin")
+    VectorStorage(f"{data_location}/netzpolitik_vs_title_with_first_paragraph.bin", num_elements) \
+        .add_items_from_file(articles_path, embedding_func_title_with_first_paragraph)
 
     print("Initialize netzpolitik vector storage of embeddings of pre-annotated keywords.\n")
     def embedding_func_annotated_keywords(raw):
         return fe.get_embedding_of_keywords(raw)
-    vs_annotated_k = VectorStorage()
-    vs_annotated_k.add_items_from_file(articles_path, embedding_func_annotated_keywords)
-    vs_annotated_k.save(f"{data_location}/netzpolitik_vs_annotated_k.bin")
+    VectorStorage(f"{data_location}/netzpolitik_vs_annotated_k.bin", num_elements) \
+        .add_items_from_file(articles_path, embedding_func_annotated_keywords)
 
-    print("Initialize netzpolitik vector storage of embeddings of extracted tf-idf keywords.\n")
+    print("Initialize netzpolitik vector storage of embeddings of extracted tf-idf keywords (normalized, unordered).\n")
     def embedding_func_tf_idf_keywords(raw):
-        raw["keywords"] = parser.get_keywords_tf_idf("netzpolitik", raw["id"])
-        return fe.get_embedding_of_keywords(raw)
-    vs_extracted_k = VectorStorage()
-    vs_extracted_k.add_items_from_file(articles_path, embedding_func_tf_idf_keywords)
-    vs_extracted_k.save(f"{data_location}/netzpolitik_vs_extracted_k.bin")
+        keyw = parser.get_keywords_tf_idf(index, raw["id"])
+        return fe.get_embedding_of_keywords(keyw)
+    VectorStorage(f"{data_location}/netzpolitik_vs_extracted_k_normalized.bin", num_elements) \
+        .add_items_from_file(articles_path, embedding_func_tf_idf_keywords)
 
-    print("Initialize netzpolitik vector storage of embeddings of titles and section titles.\n")
-    def embedding_func_section_titles(raw):
-        return fe.get_embedding_of_title_with_section_titles(raw)
-    vs_section_titles = VectorStorage()
-    vs_section_titles.add_items_from_file(articles_path, embedding_func_section_titles)
-    vs_section_titles.save(f"{data_location}/netzpolitik_vs_section_titles.bin")
+    print("Initialize netzpolitik vector storage of embeddings of extracted tf-idf keywords (denormalized, unordered).\n")
+    def embedding_func_tf_idf_keywords_denormalized(raw):
+        keyw = parser.get_keywords_tf_idf_denormalized(index, raw["id"], raw["body"], keep_order=False)
+        return fe.get_embedding_of_keywords(keyw)
+    VectorStorage(f"{data_location}/netzpolitik_vs_extracted_k_denormalized.bin", num_elements) \
+        .add_items_from_file(articles_path, embedding_func_tf_idf_keywords_denormalized)
 
-    print("Initialize netzpolitik vector storage of embeddings of titles.\n")
-    def embedding_func_titles(raw):
-        return fe.get_embedding_of_title(raw)
-    vs_titles = VectorStorage()
-    vs_titles.add_items_from_file(articles_path, embedding_func_titles)
-    vs_titles.save(f"{data_location}/netzpolitik_vs_titles.bin")
+    print("Initialize netzpolitik vector storage of embeddings of extracted tf-idf keywords (denormalized, order preserved).\n")
+    def embedding_func_tf_idf_keywords_denormalized_ordered(raw):
+        keyw = parser.get_keywords_tf_idf_denormalized(index, raw["id"], raw["body"], keep_order=True)
+        return fe.get_embedding_of_keywords(keyw)
+    VectorStorage(f"{data_location}/netzpolitik_vs_extracted_k_denormalized_ordered.bin", num_elements) \
+        .add_items_from_file(articles_path, embedding_func_tf_idf_keywords_denormalized_ordered)
--- wapo/index_vs.py
+++ wapo/index_vs.py
@@ -10,28 +10,54 @@ import json
 if __name__ == "__main__":
     es = Elasticsearch()
     parser = ParserWAPO(es)
-    em = EmbeddingModel(lang="en")
+    lang = "en"
+    em = EmbeddingModel(lang)
     fe = FeatureExtraction(em, parser)
     index = "wapo_clean"
     data_location = f"{os.path.abspath(os.path.join(__file__ , os.pardir, os.pardir))}/data"
     articles_path = f"{data_location}/TREC_Washington_Post_collection.v3.jl"
+    num_elements = 500000
 
-    print("Initialize WAPO vector storage of embeddings of titles w/ first paragraph.\n")
-    def embedding_func_twfp(raw):
+    print("Initialize WAPO vector storage of embeddings of title.\n")
+    def embedding_func_title(raw):
+        article = parser.parse_article(raw)
+        return fe.get_embedding_of_title(article)
+    VectorStorage(f"{data_location}/wapo_vs_title.bin", num_elements) \
+        .add_items_from_file(articles_path, embedding_func_title)
+
+    print("Initialize WAPO vector storage of embeddings of title and section titles.\n")
+    def embedding_func_title_with_section_titles(raw):
+        article = parser.parse_article(raw)
+        return fe.get_embedding_of_title_with_section_titles(article)
+    VectorStorage(f"{data_location}/wapo_vs_title_with_section_titles.bin", num_elements) \
+        .add_items_from_file(articles_path, embedding_func_title_with_section_titles)
+
+    print("Initialize WAPO vector storage of embeddings of title with first paragraph.\n")
+    def embedding_func_title_with_first_paragraph(raw):
         article = parser.parse_article(raw)
         return fe.get_embedding_of_title_with_first_paragraph(article)
-    vs_twfp = VectorStorage(num_elements=500000)
-    vs_twfp.add_items_from_file(articles_path, embedding_func_twfp)
-    vs_twfp.save(f"{data_location}/wapo_vs_twfp.bin")
+    VectorStorage(f"{data_location}/wapo_vs_title_with_first_paragraph.bin", num_elements) \
+        .add_items_from_file(articles_path, embedding_func_title_with_first_paragraph)
 
-    print("Initialize WAPO vector storage of embeddings of keywords.\n")
-    def embedding_func_keywords(raw):
+    print("Initialize WAPO vector storage of embeddings of extracted tf-idf keywords (normalized, unordered).\n")
+    def embedding_func_tf_idf_keywords(raw):
+        keyw = parser.get_keywords_tf_idf(index, raw["id"])
+        return fe.get_embedding_of_keywords(keyw)
+    VectorStorage(f"{data_location}/wapo_vs_extracted_k_normalized.bin", num_elements) \
+        .add_items_from_file(articles_path, embedding_func_tf_idf_keywords)
+
+    print("Initialize WAPO vector storage of embeddings of extracted tf-idf keywords (denormalized, unordered).\n")
+    def embedding_func_tf_idf_keywords_denormalized(raw):
         article = parser.parse_article(raw)
-        combined = [article["title"], article["text"]]
-        combined_text = " ".join([t for t in combined if t])
-        article["keywords"] = parser.get_keywords_tf_idf_denormalized(index, article_id, combined_text)
-        #article["keywords"] = parser.get_keywords_tf_idf(index, article_id)
-        return fe.get_embedding_of_keywords(article)
-    vs_keywords = VectorStorage(num_elements=500000)
-    vs_keywords.add_items_from_file(articles_path, embedding_func_keywords)
-    vs_keywords.save(f"{data_location}/wapo_vs_keywords.bin")
+        keyw = parser.get_keywords_tf_idf_denormalized(index, raw["id"], article["body"], keep_order=False)
+        return fe.get_embedding_of_keywords(keyw)
+    VectorStorage(f"{data_location}/wapo_vs_extracted_k_denormalized.bin", num_elements) \
+        .add_items_from_file(articles_path, embedding_func_tf_idf_keywords_denormalized)
+
+    print("Initialize WAPO vector storage of embeddings of extracted tf-idf keywords (denormalized, order preserved).\n")
+    def embedding_func_tf_idf_keywords_denormalized_ordered(raw):
+        article = parser.parse_article(raw)
+        keyw = parser.get_keywords_tf_idf_denormalized(index, raw["id"], article["body"], keep_order=True)
+        return fe.get_embedding_of_keywords(keyw)
+    VectorStorage(f"{data_location}/wapo_vs_extracted_k_denormalized_ordered.bin", num_elements) \
+        .add_items_from_file(articles_path, embedding_func_tf_idf_keywords_denormalized_ordered)
